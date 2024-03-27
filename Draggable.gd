4cd4119cc059c250404ab451d5765193d75c1346
Jason Wodicka <jason@wodicka.net>
2024-03-27 13:26:42 -0700

    Drag behavior working outside zoom context

--- Draggable.gd
+++ Draggable.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var is_dragging = false
+var drag_offset = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,13 +9,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if is_dragging:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			position = get_viewport().get_mouse_position() - drag_offset * get_viewport_transform().affine_inverse()
+	else:
+		end_drag()
 	
 func begin_drag():
-	print("begin drag")
+	is_dragging = true;
+	var viewport_position = position * get_viewport_transform()
+	drag_offset = get_viewport().get_mouse_position() - viewport_position		
 	
 func end_drag():
-	print("end drag")
+	if is_dragging:
+		is_dragging = false
 		
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
