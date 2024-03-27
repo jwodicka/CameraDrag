06ec697948ab04624f86e6685e248cf5fd0a908d
Jason Wodicka <jason@wodicka.net>
2024-03-27 13:21:43 -0700

    Added draggable item and input-enabled it (no drag yet)

---
+++ Draggable.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func begin_drag():
+	print("begin drag")
+	
+func end_drag():
+	print("end drag")
+		
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				begin_drag()
+			else:
+				end_drag()
+
